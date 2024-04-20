e29ca32ff2f3e84c4c52f16ed02ec585df75a906
João <joaovictorgacunha@gmail.com>
2024-04-19 22:33:03 -0300

    Ultima versao atividade

--- src/main/java/br/com/applip1/controller/ProdutoController.java
+++ src/main/java/br/com/applip1/controller/ProdutoController.java
@@ -1,6 +1,5 @@
 package br.com.applip1.controller;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,10 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.applip1.cloneprof.model.Produto;
 
@@ -21,14 +18,14 @@ public class ProdutoController {
 
 	private static List<Produto> listaProdutos;
 	
-	private static int proxId = "1";
+	private static int proxId = 1;
 	
 	@GetMapping("/produtos")
 	public ResponseEntity<List<Produto>>  produtos() {
 		return ResponseEntity
 				.status(HttpStatus.OK) 
 				.body(listaProdutos); 
-	
+	}
 	
 	
 	public ResponseEntity<Produto> novoProduto(@RequestBody Produto produto) {
@@ -48,7 +45,7 @@ public class ProdutoController {
 		Produto produto = findById(id);
 		
 		if (produto == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
 		}
 		
 		return ResponseEntity
@@ -62,10 +59,11 @@ public class ProdutoController {
 				return p;
 			}
 		}
+		return null;
 	}
 	
 	@PutMapping("/produtos/{id}")
-	public Produto atualizarProduto(
+	public ResponseEntity<Object> atualizarProduto(
 			@PathVariable(value = "id") Integer id,
 			@RequestBody Produto produto) {
 		
@@ -74,7 +72,9 @@ public class ProdutoController {
 		Produto produtoEncontrado = findById(id);
 		
 		if (produtoEncontrado == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("Produto não encontrado.");
 		}
 		
 		BeanUtils.copyProperties(produto, produtoEncontrado);
@@ -85,7 +85,7 @@ public class ProdutoController {
 	}
 	
 	@DeleteMapping("/produtos/{id}")
-	public apagarProduto(
+	public ResponseEntity<Object> apagarProduto(
 			@PathVariable(value = "id") Integer id) {
 		
 		Produto produtoApagado = procurarEApagar(id);
@@ -104,7 +104,7 @@ public class ProdutoController {
 		Iterator<Produto> i = listaProdutos.iterator();
 		while(i.hasNext()) {
 			Produto p = i.next();
-			if (p.getId() = id) {
+			if (p.getId() == id) {
 				i.remove();
 				return p;
 			}
